2ccbd290036e75c19683a449adacb7a9f01cde59
Jan Mercl <0xjnml@gmail.com>
2022-09-27 20:52:14 +0200

    enlarge shell time for very slow builders (qemu openbsd/386)

--- v4/lib/all_test.go
+++ v4/lib/all_test.go
@@ -42,7 +42,7 @@ var (
 	oErr1       = flag.Bool("err1", false, "first error line only")
 	oKeep       = flag.Bool("keep", false, "keep temp directories")
 	oPanic      = flag.Bool("panic", false, "panic on miscompilation")
-	oShellTime  = flag.Duration("shelltimeout", 300*time.Second, "shell() time limit")
+	oShellTime  = flag.Duration("shelltimeout", 1800*time.Second, "shell() time limit")
 	oStackTrace = flag.Bool("trcstack", false, "")
 	oTrace      = flag.Bool("trc", false, "print tested paths.")
 	oTraceC     = flag.Bool("trcc", false, "trace TestExec transiple errors")
