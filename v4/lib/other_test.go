f2e12b9f6ac0563e74606e13f79478da56e7ccc3
Jan Mercl <0xjnml@gmail.com>
2022-10-19 08:44:00 +0200

    v4: other_test.go: disable another failing test case

--- v4/lib/other_test.go
+++ v4/lib/other_test.go
@@ -88,6 +88,9 @@ var testExecKnownFails = map[string]struct{}{
 	`assets/gcc-9.1.0/gcc/testsuite/gcc.c-torture/execute/960405-1.c`:                 {}, // BUILD FAIL
 	`assets/github.com/gcc-mirror/gcc/gcc/testsuite/gcc.c-torture/execute/960405-1.c`: {}, // BUILD FAIL
 
+	//TODO
+	`assets/gcc-9.1.0/gcc/testsuite/gcc.c-torture/execute/20011219-1.c`: {}, // BUILD FAIL
+
 	// ====================================================================
 	// Should not compile (transpile).
 	`assets/gcc-9.1.0/gcc/testsuite/gcc.c-torture/compile/pr71109.c`: {}, // COMPILE FAIL
